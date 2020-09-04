96efb94455737d6f463e04ad63211245fb46610a
Christoph Weniger <c.weniger@uva.nl>
2020-09-04 10:59:58 +0200

    Moved class based interface to swyft.py

--- swyft/old.py
+++ swyft/old.py
@@ -71,3 +71,132 @@ def update_xz(xz, network, x0, model, n_sims, lnL_th = -6, n_sub = 1000, append 
         xz = xz_new
     return xz
 
+
+class SWYFT_old:
+    """SWYFT. Convenience class around functional methods."""
+    def __init__(self, model, z_dim, x0, device = 'cpu', verbosity = True):
+        self.model = model
+        self.z_dim = z_dim
+        self.x0 = x0 
+        self.device = device
+
+        self.xz_store = []
+        self.net_store = []
+        self.loss_store = []
+        self.test_loss_store = []
+        self.verbose = verbosity
+
+    def square(self, n_train = [3000,3000,3000], lr = [1e-3,1e-4,1e-5], head = None,
+            combinations = None, p = 0.2, n_batch = 3):
+
+        # Instantiate network
+        if head is None:
+            x_dim = len(self.x0)
+        else:
+            x_dim = head(torch.tensor(self.x0).float().to(self.device)).shape[-1]
+        zdim = len(combinations)
+        pdim = len(combinations[0])
+        network = Network(x_dim, zdim, pdim = pdim, head = head, p = p).to(self.device)
+        xz = self.xz_store[-1]
+        
+        z = get_z(xz)
+        network.train()
+
+        losses = []
+        for i in range(len(lr)):
+            loss = train(network, xz, n_train = n_train[i], lr = lr[i],
+                    device = self.device, n_batch = n_batch, combinations=combinations)
+            losses += loss
+
+        network.eval()
+        out = estimate_lnL_batched(network, self.x0, z, device = self.device,
+                normalize = False, combinations = combinations)
+        return out, losses
+
+    def round(self, n_sims = 3000, n_train = [3000,3000,3000], lr = [1e-3,1e-4,1e-5],
+            head = None, combine = False, p = 0.2, n_batch = 3, threshold = 1e-6):
+        if self.verbose:
+            print("Round: ", len(self.xz_store))
+
+        # New simulations requested
+        if n_sims > 0:
+            # Generate new z
+            if self.verbose:
+                print("Generate samples from constrained prior: z~pc(z)")
+            if len(self.net_store) == 0:
+                z = sample_z(n_sims, self.z_dim)  # draw from initial prior
+            else:
+                z = iter_sample_z(n_sims, self.z_dim, self.net_store[-1],
+                        self.x0, device = self.device, verbosity =
+                        self.verbose, threshold = threshold)
+
+            # Generate new x
+            if self.verbose:
+                print("Generate corresponding draws x ~ p(x|z)")
+            xz = sample_x(self.model, z)
+
+            if combine:
+                xz =xz+self.xz_store[-1]
+        else:
+            # Take simply previous samples
+            if self.verbose:
+                print("Reusing samples from previous round.")
+            xz = self.xz_store[-1]
+
+        # Instantiate network
+        if head is None:
+            x_dim = len(self.x0)
+        else:
+            x_dim = head(torch.tensor(self.x0).float().to(self.device)).shape[-1]
+        network = Network(x_dim, self.z_dim, xz_init = xz, head = head, p = p).to(self.device)
+        network.train()
+
+        if self.verbose:
+            print("Network optimization")
+        # Perform optimization
+        if isinstance(lr, list):
+            losses = []
+            for i in range(len(lr)):
+                loss = train(network, xz, n_train =
+                        n_train[i], lr = lr[i],
+                        n_batch = n_batch)
+                losses += loss
+        else:
+            losses = train(network, xz, n_train =
+                    n_train, lr = lr, n_batch=n_batch)#, device =self.device, n_batch = n_batch)
+
+        # Store results
+        self.xz_store.append(xz)
+        self.net_store.append(network)
+        self.loss_store.append(losses)
+
+    def get_posteriors(self, nround = -1, x0 = None, error = False, z = None, n_sub = None):
+        network = self.net_store[nround]
+        if z is None:
+            z = get_z(self.xz_store[nround])
+        x0 = x0 if x0 is not None else self.x0
+        post = get_posteriors(network, x0, z, device = self.device, error = error)
+        return post
+
+    def save(self, filename):
+        """Save current state, including sampled data, loss history and fitted networks.
+
+        :param filename: Output filename, .pt format.
+        :type filename: String
+        """
+        obj = {'xz_store': self.xz_store,
+               'loss_store': self.loss_store,
+               'net_store': self.net_store
+               }
+        torch.save(obj, filename)
+
+    def load(self, filename):
+        """Load previously saved state, including sampled data, loss history and fitted networks.
+
+        :param filename: Input filename, .pt format.
+        :type filename: String
+        """
+        obj = torch.load(filename)
+        self.xz_store = obj['xz_store']
+        self.net_store = obj['net_store']
+        self.loss_store = obj['loss_store']
--- swyft/swyft.py
+++ swyft/swyft.py
@@ -5,131 +5,150 @@ import torch
 import torch.nn as nn
 from .core import *
 
+class Data(torch.utils.data.Dataset):
+    def __init__(self, xz):
+        super().__init__()
+        self.xz = xz
+
+    def __len__(self):
+        return len(self.xz)
+
+    def __getitem__(self, idx):
+        return self.xz[idx]
+
+def gen_train_data(model, nsamples, zdim, mask = None):
+    # Generate training data
+    if mask is None:
+        z = sample_hypercube(nsamples, zdim)
+    else:
+        z = sample_constrained_hypercube(nsamples, zdim, mask)
+    
+    xz = simulate_xz(model, z)
+    dataset = Data(xz)
+    
+    return dataset
+
+def trainloop(net, dataset, combinations = None, nbatch = 64, nworkers = 4,
+        max_epochs = 5, early_stopping_patience = 20, device = 'cpu'):
+    nvalid = 512
+    ntrain = len(dataset) - nvalid
+    dataset_train, dataset_valid = torch.utils.data.random_split(dataset, [ntrain, nvalid])
+    train_loader = torch.utils.data.DataLoader(dataset_train, batch_size=nbatch, num_workers=nworkers, pin_memory=True, drop_last=True)
+    valid_loader = torch.utils.data.DataLoader(dataset_valid, batch_size=nbatch, num_workers=nworkers, pin_memory=True, drop_last=True)
+    # Train!
+
+    train_loss, valid_loss = [], []
+    for i, lr in enumerate([1e-3, 1e-4, 1e-5]):
+        print(f'LR Iter {i}', end="\r")
+        tl, vl, sd = train(net, train_loader, valid_loader,
+                early_stopping_patience = early_stopping_patience, lr = lr,
+                max_epochs = max_epochs, device=device, combinations =
+                combinations)
+        vl_minimum = min(vl)
+        vl_min_idx = vl.index(vl_minimum)
+        train_loss.append(tl[:vl_min_idx + 1])
+        valid_loss.append(vl[:vl_min_idx + 1])
+        net.load_state_dict(sd)
+
+
+def posteriors(x0, net, dataset, combinations = None, device = 'cpu'):
+    x0 = torch.tensor(x0).float().to(device)
+    z = torch.stack(get_z(dataset)).to(device)
+    z = torch.stack([combine_z(zs, combinations) for zs in z])
+    lnL = get_lnL(net, x0, z)
+    return z.cpu(), lnL.cpu()
+
+
+
 class SWYFT:
-    """SWYFT. Convenience class around functional methods."""
-    def __init__(self, model, z_dim, x0, device = 'cpu', verbosity = True):
+    def __init__(self, x0, model, zdim, device = 'cpu'):
+        self.x0 = x0
         self.model = model
-        self.z_dim = z_dim
-        self.x0 = x0 
+        self.zdim = zdim
         self.device = device
 
-        self.xz_store = []
-        self.net_store = []
-        self.loss_store = []
-        self.test_loss_store = []
-        self.verbose = verbosity
+        # Each data_store entry has a corresponding mask entry
+        # TODO: Replace with datastore eventually
+        self.mask_store = []
+        self.data_store = []
 
-    def square(self, n_train = [3000,3000,3000], lr = [1e-3,1e-4,1e-5], head = None,
-            combinations = None, p = 0.2, n_batch = 3):
+        # NOTE: Each trained network goes together with evaluated posteriors (evaluated on x0)
+        self.post1d_store = []
+        self.net1d_store = []
 
-        # Instantiate network
-        if head is None:
-            x_dim = len(self.x0)
-        else:
-            x_dim = head(torch.tensor(self.x0).float().to(self.device)).shape[-1]
-        zdim = len(combinations)
-        pdim = len(combinations[0])
-        network = Network(x_dim, zdim, pdim = pdim, head = head, p = p).to(self.device)
-        xz = self.xz_store[-1]
-        
-        z = get_z(xz)
-        network.train()
+        # NOTE: We separate N-dim posteriors since they are not used (yet) for refining training data
+        self.postNd_store = []
+        self.netNd_store = []
 
-        losses = []
-        for i in range(len(lr)):
-            loss = train(network, xz, n_train = n_train[i], lr = lr[i],
-                    device = self.device, n_batch = n_batch, combinations=combinations)
-            losses += loss
+    def train(self):
+        """Train 1-dim posteriors."""
+        # Use most recent dataset by default
+        dataset = self.data_store[-1]
 
-        network.eval()
-        out = estimate_lnL_batched(network, self.x0, z, device = self.device,
-                normalize = False, combinations = combinations)
-        return out, losses
+        # Initialize neural network
+        ydim = len(self.x0)  # TODO: Needs to be made dynamic and head-dependent
+        net = Network(ydim = ydim, pnum = self.zdim, pdim = 1).to(self.device)
 
-    def round(self, n_sims = 3000, n_train = [3000,3000,3000], lr = [1e-3,1e-4,1e-5],
-            head = None, combine = False, p = 0.2, n_batch = 3, threshold = 1e-6):
-        if self.verbose:
-            print("Round: ", len(self.xz_store))
+        # Train
+        trainloop(net, dataset, device = self.device)
 
-        # New simulations requested
-        if n_sims > 0:
-            # Generate new z
-            if self.verbose:
-                print("Generate samples from constrained prior: z~pc(z)")
-            if len(self.net_store) == 0:
-                z = sample_z(n_sims, self.z_dim)  # draw from initial prior
-            else:
-                z = iter_sample_z(n_sims, self.z_dim, self.net_store[-1],
-                        self.x0, device = self.device, verbosity =
-                        self.verbose, threshold = threshold)
-
-            # Generate new x
-            if self.verbose:
-                print("Generate corresponding draws x ~ p(x|z)")
-            xz = sample_x(self.model, z)
-
-            if combine:
-                xz =xz+self.xz_store[-1]
-        else:
-            # Take simply previous samples
-            if self.verbose:
-                print("Reusing samples from previous round.")
-            xz = self.xz_store[-1]
-
-        # Instantiate network
-        if head is None:
-            x_dim = len(self.x0)
-        else:
-            x_dim = head(torch.tensor(self.x0).float().to(self.device)).shape[-1]
-        network = Network(x_dim, self.z_dim, xz_init = xz, head = head, p = p).to(self.device)
-        network.train()
-
-        if self.verbose:
-            print("Network optimization")
-        # Perform optimization
-        if isinstance(lr, list):
-            losses = []
-            for i in range(len(lr)):
-                loss = train(network, xz, n_train =
-                        n_train[i], lr = lr[i],
-                        n_batch = n_batch)
-                losses += loss
-        else:
-            losses = train(network, xz, n_train =
-                    n_train, lr = lr, n_batch=n_batch)#, device =self.device, n_batch = n_batch)
+        # Get 1-dim posteriors
+        zgrid, lnLgrid = posteriors(self.x0, net, dataset, device = self.device)
 
         # Store results
-        self.xz_store.append(xz)
-        self.net_store.append(network)
-        self.loss_store.append(losses)
+        self.net1d_store.append(net)
+        self.post1d_store.append((zgrid, lnLgrid))
 
-    def get_posteriors(self, nround = -1, x0 = None, error = False, z = None, n_sub = None):
-        network = self.net_store[nround]
-        if z is None:
-            z = get_z(self.xz_store[nround])
-        x0 = x0 if x0 is not None else self.x0
-        post = get_posteriors(network, x0, z, device = self.device, error = error)
-        return post
+    def data(self, nsamples = 3000):
+        """Generate training data on constrained prior."""
+        if len(self.mask_store) == 0:
+            mask = None
+        else:
+            last_net = self.net1d_store[-1]
+            mask = Mask(last_net, torch.tensor(self.x0).float().to(self.device), 1e-8)
 
-    def save(self, filename):
-        """Save current state, including sampled data, loss history and fitted networks.
+        dataset = gen_train_data(self.model, nsamples, self.zdim, mask = mask)
 
-        :param filename: Output filename, .pt format.
-        :type filename: String
-        """
-        obj = {'xz_store': self.xz_store,
-               'loss_store': self.loss_store,
-               'net_store': self.net_store
-               }
-        torch.save(obj, filename)
+        # Store dataset and mask
+        self.mask_store.append(mask)
+        self.data_store.append(dataset)
 
-    def load(self, filename):
-        """Load previously saved state, including sampled data, loss history and fitted networks.
+    def run(self, nrounds = 1):
+        """Iteratively generating training data and train 1-dim posteriors."""
+        for i in range(nrounds):
+            self.data()
+            self.train()
 
-        :param filename: Input filename, .pt format.
-        :type filename: String
-        """
-        obj = torch.load(filename)
-        self.xz_store = obj['xz_store']
-        self.net_store = obj['net_store']
-        self.loss_store = obj['loss_store']
+    def comb(self, combinations):
+        """Generate N-dim posteriors."""
+        # Use by default data from last 1-dim round
+        dataset = self.data_store[-1]
+
+        # Generate network
+        pdim = len(combinations[0])
+        pnum = len(combinations)
+        net = Network(ydim = 3, pnum = pnum, pdim = pdim).to(self.device)
+
+        # Train!
+        trainloop(net, dataset, combinations = combinations, device = self.device)
+
+        # Get posteriors and store them internally
+        zgrid, lnLgrid = posteriors(self.x0, net, dataset, combinations =
+                combinations, device = self.device)
+
+        self.postNd_store.append((combinations, zgrid, lnLgrid))
+        self.netNd_store.append(net)
+
+    def posterior(self, indices):
+        """Return generated posteriors."""
+        if isinstance(indices, int):
+            i = indices
+            return self.post1d_store[-1][0][:,i], self.post1d_store[-1][1][:,i]
+        else:
+            for i in range(len(self.postNd_store)):
+                combinations = self.postNd_store[i][0]
+                if indices in combinations:
+                    j = combinations.index(indices)
+                    return self.postNd_store[i][1][:,j], self.postNd_store[i][2][:,j]
+            print("WARNING: Did not find requested parameter combination.")
+            return None
