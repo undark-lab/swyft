d85a87a84837b369abdc7679b3567554039dd55c
Christoph Weniger <c.weniger@uva.nl>
2020-09-22 13:24:07 +0200

    autodocs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -20,7 +20,7 @@ import os
 import sys
 import sphinx_rtd_theme
 
-sys.path.insert(0, os.path.abspath('../../swyft/'))
+sys.path.insert(0, os.path.abspath('../../'))
 
 autodoc_mock_imports = ['yaml', 'pyro', 'numpy', 'torch', 'pykeops', 'scipy']
 
