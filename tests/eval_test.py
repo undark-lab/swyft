694108c4f380e74a0706f4d7bfbda33693672eb6
bkmi <12955549+bkmi@users.noreply.github.com>
2020-11-23 20:21:32 +0100

    minor stylefix

--- tests/eval_test.py
+++ tests/eval_test.py
@@ -8,21 +8,17 @@ from swyft.eval import eval_net, get_ratios
 from .estimation_test import sim_repeat_noise, setup_points, Head
 
 
-def make_ground_truth(points):
-    z0 = np.random.rand(points.zdim)
-    x0 = sim_repeat_noise(z0, points.xshape[0])
-    z0 = z0[None, :, None]
-    x0 = x0
-    return z0, x0
-
-
 class TestEvaluation:
     @staticmethod
     def setup_re_x0_z0_points():
         _, points = setup_points()
         head = Head(*Head.featurize(points.xshape))
         re = RatioEstimator(points, head=head)
-        z0, x0 = make_ground_truth(points)
+
+        z0 = np.random.rand(points.zdim)
+        x0 = sim_repeat_noise(z0, points.xshape[0])
+        z0 = z0[None, :, None]
+        x0 = x0
         return re, x0, z0, points
 
     @pytest.mark.parametrize("training", (True, False))
@@ -40,7 +36,7 @@ class TestEvaluation:
 
     @pytest.mark.parametrize("training", (True, False))
     def test_get_ratios_network_state(self, training):
-        re, x0, z0, points = TestEvaluation.setup_re_x0_z0_points()
+        re, x0, _, points = TestEvaluation.setup_re_x0_z0_points()
 
         if training:
             re.net.train()
