57c763108907c444b9476fb79385bc7db4809439
Christoph Weniger <c.weniger@uva.nl>
2021-01-07 16:22:29 +0100

    Add new intensity models.

--- swyft/intensity.py
+++ swyft/intensity.py
@@ -2,8 +2,10 @@
 from pathlib import Path
 import pickle
 
+import torch
 import torch.nn as nn
 import numpy as np
+from sklearn.neighbors import BallTree
 
 from .types import Shape, Union, Sequence, PathType, Array, Device, Optional
 from .eval import eval_net
@@ -307,3 +309,252 @@ def get_factor_mask_from_intervals(
         raise ValueError(
             "Intervals must be a list of intervals or a list of a list of intervals."
         )
+
+
+# ----------------------------------
+# ----------------------------------
+# ----------------------------------
+
+class BallMask:
+    def __init__(self, points, scale = 1.0):
+        """Simple mask based on coverage balls around inducing points.
+
+        Args:
+            points (Array): shape (num_points, n_dim)
+            scale (float): Scale ball size (default 1.0)
+        """
+        assert len(points.shape) == 2
+        self.X = points
+        self.dim = self.X.shape[-1]
+        self.bt = BallTree(self.X, leaf_size = 2)
+        self.epsilon = self._set_epsilon(self.X, self.bt, scale)
+        self.volume = self._get_volume(self.X, self.epsilon, self.bt)
+       
+    @classmethod
+    def load(cls, state_dict):
+        obj = cls.__new__(cls)
+        obj.X = state_dict['points']
+        assert len(obj.X.shape) == 2
+        obj.dim = obj.X.shape[-1]
+        obj.epsilon = state_dict['epsilon']
+        obj.volume = state_dict['volume']
+        obj.bt = BallTree(obj.X, leaf_size = 2)
+        return obj
+        
+    def dump(self):
+        return dict(masktype = "BallMask", points=self.X, epsilon=self.epsilon, volume=self.volume)
+        
+    @staticmethod
+    def _set_epsilon(X, bt, scale):
+        dims = X.shape[-1]
+        k = [4, 5, 6]
+        dist, ind = bt.query(X, k = k[dims-1])  # 4th NN
+        epsilon = np.median(dist[:,-1])*scale*1.5
+        return epsilon
+    
+    @staticmethod
+    def _get_volume(X, epsilon, bt):
+        N = 30
+        vol_est = []
+        d = X.shape[-1]
+        for i in range(N):
+            n = np.random.randn(*X.shape)  
+            norm = (n**2).sum(axis=1)**0.5
+            n = n/norm.reshape(-1, 1)
+            r = np.random.rand(len(X))**(1/d)*epsilon
+            Y = X + n*r.reshape(-1, 1)
+            in_bounds = ((Y>=0.) & (Y<=1.)).prod(axis=1, dtype='bool')
+            Y = Y[in_bounds]
+            counts = bt.query_radius(Y, epsilon, count_only = True)
+            #print(counts.min(), counts.max(), len(counts), counts.mean())
+            vol_est.append(np.pi*epsilon**2 * sum(1./counts))
+        vol_est = np.array(vol_est)
+        return vol_est.mean(), vol_est.std()
+    
+    def log_prob(self, points):
+        return np.where(self.__call__(points), -np.log(self.volume), -np.inf)
+    
+    def sample(self, N):
+        counter = 0
+        samples = []
+        d = self.X.shape[-1]
+        while counter < N:
+            n = np.random.randn(*self.X.shape)  
+            norm = (n**2).sum(axis=1)**0.5
+            n = n/norm.reshape(-1, 1)
+            r = np.random.rand(len(self.X))**(1/d)*self.epsilon
+            Y = self.X + n*r.reshape(-1, 1)
+            in_bounds = ((Y>=0.) & (Y<=1.)).prod(axis=1, dtype='bool')
+            Y = Y[in_bounds]
+            counts = self.bt.query_radius(Y, r=self.epsilon, count_only = True)
+            p = 1./counts
+            w = np.random.rand(len(p))
+            Y = Y[p >= w]
+            samples.append(Y)
+            counter += len(Y)
+        samples = np.vstack(samples)
+        ind = np.random.choice(range(len(samples)), size = N, replace = False)
+        return samples[ind]
+
+    def __call__(self, points):
+        """Evaluate points in mask."""
+        dist, ind = self.bt.query(points, k = 1)
+        return self.M[ind][:,0]
+
+
+class ComboMask:
+    def __init__(self, samplers):
+        """Combination of lower dimensional masks.
+
+        Args:
+            samplers (dict): Dictionary of masks, mapping parameter names
+            (strings or tuple of strings) to masks.
+
+        Example:
+            samplers = {"x": mask_1d, ("y", "z"): mask_2d}
+        """
+        self.samplers = samplers
+        self.masks = None  # TODO: Implement additional masks
+        
+    def sample(self, N):
+        result = {}
+        for key in self.samplers.keys():
+            if isinstance(key, str):
+                result[key] = self.samplers[key].sample(N)[:,0]
+            elif isinstance(key, tuple):
+                for i, k in enumerate(key):
+                    result[k] = self.samplers[key].sample(N)[:,i]
+            else:
+                raise KeyError("ComboMask keys must be strings or string tuples.")
+        return result
+            
+    def volume(self):
+        volume = 1.
+        for key in self.samplers:
+            volume *= self.samplers[key].volume[0]
+        return volume
+    
+    def __call__(self, X):
+        raise NotImplementedError
+    
+    @classmethod
+    def load(cls, state_dict):
+        samplers = {}
+        for key, value in state_dict['samplers'].items():
+            samplers[key] = BallMask.load(value)
+        return cls(samplers)
+    
+    def dump(self):
+        samplers = {}
+        for key, value in self.samplers.items():
+            samplers[key] = value.dump()
+        return dict(samplers = samplers)
+
+
+class Prior1d:
+    def __init__(self, tag, *args):
+        self.tag = tag
+        self.args = args
+        if tag == 'normal':
+            log, scale = args[0], args[1]
+            self.prior = torch.distributions.Normal(log, scale)
+        elif tag == 'uniform':
+            x0, x1 = args[0], args[1]
+            self.prior = torch.distributions.Uniform(x0, x1)
+        else:
+            raise KeyError("Tag unknown")
+            
+    def sample(self, N):
+        return self.prior.sample((N,))
+    
+    def log_prob(self, value):
+        return self.prior.log_prob(value)
+    
+    def to_cube(self, value):
+        return self.prior.cdf(value)
+    
+    def from_cube(self, value):
+        value = torch.tensor(value)
+        return self.prior.icdf(value).numpy()
+    
+    def dump(self):
+        return dict(tag = self.tag, args = self.args)
+    
+    @classmethod
+    def load(cls, state_dict):
+        return cls(state_dict['tag'], *state_dict['args'])
+
+
+class Prior:
+    def __init__(self, priors_dict, mask = None):
+        self.priors_dict = priors_dict
+        self.mask = mask
+
+        self._setup_priors()
+
+    def _setup_priors(self):
+        result = {}
+        for key, value in self.priors_dict.items():
+            result[key] = Prior1d(value[0], *value[1:])
+        self.priors = result
+
+    def sample(self, N):
+        if self.mask is None:
+            return self._sample_from_priors(N)
+        else:
+            samples = self.mask.sample(N)
+            return self.from_cube(samples)
+
+    def _sample_from_priors(self, N):
+        result = {}
+        for key, value in self.priors.items():
+            result[key] = value.sample(N)
+        return result
+
+    def log_prob(self, values):
+        log_prob = []
+        for key, value in self.priors.items():
+            x = torch.tensor(values[key])
+            log_prob.append(value.log_prob(x).numpy())
+        return sum(log_prob)
+
+    def to_cube(self, X):
+        raise NotImplementedError
+
+    def from_cube(self, values):
+        result = {}
+        for key, value in values.items():
+            result[key] = self.priors[key].from_cube(value)
+        return result
+
+    def dump(self):
+        mask_dict = None if self.mask is None else self.mask.dump()
+        return dict(priors_dict = self.priors_dict, mask = mask_dict)
+
+    @classmethod
+    def load(cls, state_dict):
+        mask = None if state_dict['mask'] is None else ComboMask.load(state_dict['mask'])
+        return cls(state_dict['priors_dict'], mask = mask)
+
+
+class IntensityNew:
+    def __init__(self, prior, mu):
+        self.prior = prior
+        self.mu = mu
+        
+    def sample(self):
+        N = np.random.poisson(self.mu)
+        return self.prior.sample(N)
+    
+    def __call__(self, values):
+        return np.exp(self.prior.log_prob(values))*self.mu
+    
+    @classmethod
+    def load(cls, state_dict):
+        prior = Prior.load(state_dict["prior"])
+        mu = state_dict["mu"]
+        return cls(prior, mu)
+    
+    def dump(self):
+        prior = self.prior.dump()
+        return dict(prior = prior, mu = self.mu)
