92bdba40ffee1f62e735257856c2c2773467de31
Christoph Weniger <c.weniger@uva.nl>
2021-02-19 08:41:24 +0100

    Move development material to dev folder

---
+++ dev/simulate.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+import sys
+import torch
+import simulators
+
+def main():
+    try:
+        infile = sys.argv[1]
+        outfile = sys.argv[2]
+    except:
+        print("Usage: simulate.py infile outfile")
+    model = simulators.model_FermiV1
+    params = torch.load(infile)
+    obs = model(params)
+    torch.save(obs, outfile)
+
+if __name__ == "__main__":
+    main()
