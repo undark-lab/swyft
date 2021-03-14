b1da29c76913260dc34b4f2d109db74fe09e3d31
rogerkuou <rogerkuou@gmail.com>
2021-03-14 07:15:57 +0100

    add noise function

--- swyft/utils/simulator.py
+++ swyft/utils/simulator.py
@@ -15,8 +15,10 @@ import torch
 
 from swyft.types import Array
 
+
 class Simulator:
     """ Setup and run the simulator engine """
+
     def __init__(self, simulator, noise=None):
         self.simulator = simulator
         self.noise = noise
@@ -44,12 +46,18 @@ class Simulator:
                                parameters are divided for the parallelization
                                (default is about 100)
         """
-        # Parameter dict to list 
-        z = [{k: v[i] for k, v in prior.sample(n_samples).items()} for i in range(n_samples)]
-        
+        # Parameter dict to list
+        z = [{k: v[i] for k, v in prior.sample(n_samples).items()}
+             for i in range(n_samples)]
+
         bag = db.from_sequence(z, npartitions=npartitions)
-        bag = bag.map(self.simulator)
-        return bag.compute(scheduler=self.client or 'processes')
+        x = bag.map(self.simulator).compute(scheduler=self.client or 'processes')
+
+        if self.noise is None:
+            return x
+        else:
+            bag2 = db.from_sequence(x, npartitions=npartitions)
+            return bag2.map(self.noise).compute(scheduler=self.client or 'processes')
 
     @classmethod
     def from_command(cls, command, set_input_method, get_output_method,
@@ -88,4 +96,3 @@ class Simulator:
             return output
 
         return cls(simulator=simulator)
-
