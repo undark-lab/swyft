3a3c979890be448451c0abea8601f60fdd38989b
Christoph Weniger <c.weniger@uva.nl>
2021-04-05 15:47:47 +0200

    Polish prior.py

--- swyft/marginals/prior.py
+++ swyft/marginals/prior.py
@@ -1,22 +1,39 @@
-from typing import Dict, Sequence, Tuple, Union
-
 import numpy as np
-import torch
-
-from swyft.marginals.bounds import BallsBound, Bound, UnitCubeBound
-from swyft.types import Array, PriorConfig
-from swyft.utils import array_to_tensor, depth, tensor_to_array
+from swyft.marginals.bounds import Bound, UnitCubeBound
 
 class BoundedPrior:
+    """Prior with bounds."""
     def __init__(self, ptrans, bound):
+        """Instantiate BoundedPrior.
+
+        Args:
+            ptrans (PriorTransform): Map from hypercube to physical parameters.
+            bound (Bound): Bound on hypercube.
+        """
         self.ptrans = ptrans
         self.bound = bound
 
     def sample(self, N): 
+        """Sample from bounded prior.
+
+        Args:
+            N (int): Number of samples to return
+
+        Returns:
+            Samples (np.ndarray), (N, zdim)
+        """
         u = self.bound.sample(N)
         return self.ptrans.v(u)
 
     def log_prob(self, v): 
+        """Evaluate log probability of pdf.
+
+        Args:
+            v (2-dim np.ndarray): (N, zdim) parameter points.
+
+        Returns:
+            log_prob (np.ndarray, (N,))
+        """
         u = self.ptrans.u(v)
         b = np.where(u.sum(axis=-1) == np.inf, 0., self.bound(u))
         log_prob = np.where(b == 0., -np.inf, self.ptrans.log_prob(v).sum(axis=-1) - np.log(self.bound.volume))
@@ -31,63 +48,96 @@ class BoundedPrior:
         bound = Bound.from_state_dict(state_dict['bound'])
         return cls(ptrans, bound)
 
+
 class Prior(BoundedPrior):
     def __init__(self, ptrans):
-        bound = UnitCubeBound(ptrans.ndim)
+        """Generate prior without bounds.  Usually used for initial unbounded prior.
+
+        Args:
+            ptrans (PriorTransform)
+        """
+        bound = UnitCubeBound(ptrans.zdim)
         super().__init__(ptrans, bound)
 
+
 class PriorTransform:
-    def __init__(self, ptrans, ndim, n_steps= 10000):
+    def __init__(self, uv, zdim, n = 10000):
         """Prior transformation object.  Maps hypercube on physical parameters.
 
-        Args
+        Args:
+            uv (callable): Function u->v
+            zdim (int): Number of parameters
+            n (int): Number of discretization points.
         """
-        self._ndim = ndim
-        self._grid = np.linspace(0, 1., n_steps)
-        self._table = self._generate_table(ptrans, self._grid, ndim, n_steps)
+        self._zdim = zdim
+        self._grid = np.linspace(0, 1., n)
+        self._table = self._generate_table(uv, self._grid, zdim, n)
 
     @staticmethod
-    def _generate_table(ptrans, grid, ndim, n_steps):
+    def _generate_table(uv, grid, zdim, n):
         table = []
         for x in grid:
-            table.append(ptrans(np.ones(ndim)*x))
+            table.append(uv(np.ones(zdim)*x))
         return np.array(table).T
 
     @property
-    def ndim(self):
-        return self._ndim
+    def zdim(self):
+        """Number of parameters."""
+        return self._zdim
 
     def u(self, v):
-        """CDF, mapping v->u"""
+        """Map onto hypercube: v -> u
+
+        Args:
+            v (np.array): (N, zdim) physical parameter array
+
+        Returns:
+            u (np.array): (N, zdim) hypercube parameter array
+        """
         u = np.empty_like(v)
-        for i in range(self._ndim):
+        for i in range(self._zdim):
             u[:,i] = np.interp(v[:,i], self._table[i], self._grid, left = np.inf, right = np.inf)
         return u
 
     def v(self, u):
-        """Inverse CDF, mapping u->v"""
+        """Map from hypercube: u -> v
+
+        Args:
+            u (np.array): (N, zdim) hypercube parameter array
+
+        Returns:
+            v (np.array): (N, zdim) physical parameter array
+        """
         v = np.empty_like(u)
-        for i in range(self._ndim):
+        for i in range(self._zdim):
             v[:,i] = np.interp(u[:,i], self._grid, self._table[i], left = np.inf, right = np.inf)
         return v
 
     def log_prob(self, v, du = 1e-6):
-        """log prior(v)"""
+        """Log probability.
+
+        Args:
+            v (np.array): (N, zdim) physical parameter array
+            du (float): Step-size of numerical derivatives
+
+        Returns:
+            log_prob (np.array): (N, zdim) factors of pdf
+        """
         dv = np.empty_like(v)
         u = self.u(v)
-        for i in range(self._ndim):
+        for i in range(self._zdim):
             dv[:,i] = np.interp(u[:,i] +(du/2), self._grid, self._table[i], left = None, right = None)
             dv[:,i] -= np.interp(u[:,i]-(du/2), self._grid, self._table[i], left = None, right = None)
         log_prob = np.where(u == np.inf, -np.inf, np.log(du) - np.log(dv+1e-300))
         return log_prob
 
     def state_dict(self):
-        return dict(table=self._table, grid=self._grid, ndim=self._ndim)
+        return dict(table=self._table, grid=self._grid, zdim=self._zdim)
 
     @classmethod
     def from_state_dict(cls, state_dict):
         obj = cls.__new__(cls)
-        obj._ndim = state_dict['ndim']
+        obj._zdim = state_dict['zdim']
         obj._grid = state_dict['grid']
         obj._table = state_dict['table']
         return obj
