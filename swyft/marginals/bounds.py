eaf16ef97b27f2a41e7a6b0e3a3e04cb4b5587f5
Christoph Weniger <c.weniger@uva.nl>
2021-04-05 16:31:10 +0200

    Fix RectangleBound

--- swyft/marginals/bounds.py
+++ swyft/marginals/bounds.py
@@ -2,46 +2,165 @@ import numpy as np
 from sklearn.neighbors import BallTree
 from swyft.inference.ratioestimation import IsolatedRatio
 
-
+# FIXME: Finalize
 class Bound:
     def __init__(self):
         pass
 
+    @property
+    def volume(self):
+        """Volume of the bound region."""
+        raise NotImplementedError
+
+    @property
+    def udim(self):
+        """Number of dimensions."""
+        raise NotImplementedError
+
+    def sample(self, N):
+        """Sample.
+
+        Args:
+            N (int): Numbe of samples.
+
+        Returns:
+            s (N x udim np.ndarray)
+        """
+        raise NotImplementedError
+
+    def __call__(self, u):
+        """Check whether parameters are within bounds.
+
+        Args:
+            u (N x udim np.ndarray): Parameters on hypercube
+        """
+        raise NotImplementedError
+
     @classmethod
     def from_state_dict(cls, state_dict):
+        """Instantiate Bound object based on state_dict.
+
+        Args:
+            state_dict (dict): State dictionary
+        """
         tag = state_dict['tag']
-        if tag == 'CompositBound':
-            return CompositBound.from_state_dict(state_dict)
-        elif tag == 'UnitCubeBound':
+        if tag == 'UnitCubeBound':
             return UnitCubeBound.from_state_dict(state_dict)
+        elif tag == 'RectangleBound':
+            return RectangleBound.from_state_dict(state_dict)
         elif tag == 'BallsBound':
             return BallsBound.from_state_dict(state_dict)
+        elif tag == 'CompositBound':
+            return CompositBound.from_state_dict(state_dict)
         else:
             raise KeyError
 
     @classmethod
-    def from_RatioCollection(cls, rc, obs, th, zdim):
-        return CompositBound.from_RatioCollection(rc, obs, th, zdim)
+    def from_RatioCollection(cls, rc, obs, th, udim):
+        return CompositBound.from_RatioCollection(rc, obs, th, udim)
 
 
-class UnitCubeBound:
-    def __init__(self, ndim):
-        self.ndim = ndim
-        self.volume = 1.
+class UnitCubeBound(Bound):
+    """The unit hypercube bound."""
+    def __init__(self, udim):
+        """Initialize unit hypercube bound.
+
+        Args:
+            udim (int): Number of parameters.
+        """
+        self._udim = udim
+        self._volume = 1.
+
+    @property
+    def volume(self):
+        return self._volume
+
+    @property
+    def udim(self):
+        return self._udim
 
     def sample(self, N):
-        return np.random.rand(N, self.ndim)
+        return np.random.rand(N, self.udim)
 
     def __call__(self, u):
         b = np.where(u <= 1., np.where(u >= 0., 1., 0.), 0.)
         return b.prod(axis=-1)
 
     def state_dict(self):
-        return dict(tag='UnitCubeBound', ndim=self.ndim)
+        return dict(tag='UnitCubeBound', udim=self.udim)
 
     @classmethod
     def from_state_dict(cls, state_dict):
-        return cls(state_dict['ndim'])
+        return cls(state_dict['udim'])
+
+
+class RectangleBound(Bound):
+    def __init__(self, rec_bounds):
+        """Rectangle bound.
+
+        Args:
+            rec_bounds (n x 2 np.ndarray): list of (u_min, u_max) values.
+
+        Note: 0 <= u_min < u_max  <= 1.
+        """
+        self._rec_bounds = rec_bounds
+
+    @property
+    def volume(self):
+        V = 1.
+        for i in range(self.udim):
+            V *= self._rec_bounds[i,1] - self._rec_bounds[i,0]
+        return V
+
+    @property
+    def udim(self):
+        return len(self._rec_bounds)
+            
+    def sample(self, N):
+        u = np.random.rand(N, self.udim)
+        for i in range(self.udim):
+            u[:,i] *= self._rec_bounds[i,1]-self._rec_bounds[i,0]
+            u[:,i] += self._rec_bounds[i,0]
+        return u
+
+    def __call__(self, z):
+        m = np.ones(len(z))
+        for k, v in self._rec_bounds.items():
+            m *= np.where(z[:,k] >= v[0], np.where(z[:,k] <= v[1], 1., 0.), 0.)
+        return m > 0.5
+
+    @classmethod
+    def from_RatioCollection(cls, rc, obs, bound, th = -13, n = 10000):
+        """Generate new RectangleBound object based on RatioCollection.
+
+        Args:
+            rc (RatioCollection): RatioCollection to evaluate.
+            obs (dict): Reference observation.
+            bound (Bound): Bound of RatioCollection.
+            th (float): Threshold value, default -13
+            n (int): Number of random samples from bound to determine parameter boundaries.
+
+        Note: All components of the RatioCollection will be used.  Avoid overlapping ratios.
+        """
+        udim = bound.udim
+        u = bound.sample(n)
+        ratios = rc.ratios(obs, u)
+        res = np.zeros((udim, 2))
+        res[:,1] = 1.
+        for k, v in ratios.items():
+            for i in k:
+                us = u[:,i][v-v.max() > th]
+                res[i, 0] = us.min()
+                res[i, 1] = us.max()
+        
+        return cls(res)
+
+    def state_dict(self):
+        return dict(tag='RectangleBound', rec_bounds=self._rec_bounds)
+
+    @classmethod
+    def from_state_dict(cls, state_dict):
+        return cls(state_dict['rec_bounds'])
 
 
 class BallsBound:
@@ -154,55 +273,15 @@ class BallsBound:
         return obj
 
 
-class RectangleBound:
-    def __init__(self, bounds, zdim):
-        self._bounds = bounds
-        self._zdim = zdim
-
-    @property
-    def volume(self):
-        V = 1.
-        for k, v in self._bounds.items():
-            V *= (v[1] - v[0])
-        return V
-            
-    def sample(self, N):
-        u = np.random.rand(N, self._zdim)
-        for k, v in self._bounds.items():
-            u[:,k] *= v[1]-v[0]
-            u[:,k] += v[0]
-        return u
-
-    def __call__(self, z):
-        m = np.ones(len(z))
-        for k, v in self._bounds.items():
-            m *= np.where(z[:,k] >= v[0], np.where(z[:,k] <= v[1], 1., 0.), 0.)
-        return m > 0.5
-
-    @classmethod
-    def from_RatioCollection(cls, rc, obs, th, zdim):
-        bound = UnitCubeBound(zdim)
-        N = 10000
-        u = bound.sample(N)
-        ratios = rc.ratios(obs, u)
-        res = {}
-        for k, v in ratios.items():
-            if len(k) == 1:
-                us = u[:,k[0]][v > th]
-                res[k[0]] = (us.min(), us.max())
-        
-        return cls(res, zdim)
-
-
 class CompositBound:
-    def __init__(self, bounds, zdim):
+    def __init__(self, bounds, udim):
         """
         Args:
             bounds (dict): Dictionary mapping indices like (0, 3) etc --> bounds
-            zdim (int): Length of parameter vector.
+            udim (int): Length of parameter vector.
         """
         self._bounds = bounds
-        self._zdim = zdim
+        self._udim = udim
         #self.mask = self._gen_mask()
 
     def sample(self, N):
@@ -211,7 +290,7 @@ class CompositBound:
         Returns:
             Returns samples. Returns -1 where no samples where drawn.
         """
-        results = -np.ones((N, self._zdim))
+        results = -np.ones((N, self._udim))
         for k, v in self._bounds.items():
             results[:, np.array(k)] = v.sample(N)
         return results
@@ -231,28 +310,29 @@ class CompositBound:
         return sum(res) == len(res)
 
     @classmethod
-    def from_RatioCollection(cls, rc, obs, th, zdim):
+    def from_RatioCollection(cls, rc, obs, th, udim):
         bounds = {}
         # FIXME: Implement rectangle bound
-        #rb = RectangleBound.from_RatioCollection(rc, obs, th, zdim)
+        #rb = RectangleBound.from_RatioCollection(rc, obs, th, udim)
         for comb in rc.param_list:
             if len(comb) == 1:  # 1-dim case
                 bound = UnitCubeBound(1)
+                ratio = IsolatedRatio(rc, obs, comb, udim)
                 b = BallsBound.from_ratio(bound, obs, ratio)
             elif len(comb) == 2:  # 2-dim case
                 bound = UnitCubeBound(2)  # FIXME: Missing recursion
-                ratio = IsolatedRatio(rc, obs, comb, zdim)
+                ratio = IsolatedRatio(rc, obs, comb, udim)
                 b = BallsBound.from_ratio(bound, obs, ratio)
             else:
                 raise NotImplementedError
             bounds[comb] = b
 
-        return cls(bounds, zdim)
+        return cls(bounds, udim)
 
     def state_dict(self):
         state_dict = dict(
                 tag="CompositBound",
-                zdim=self._zdim,
+                udim=self._udim,
                 bounds = {k: v.state_dict() for k, v in self._bounds.items()}
                 )
         return state_dict
@@ -260,5 +340,5 @@ class CompositBound:
     @classmethod
     def from_state_dict(cls, state_dict):
         bounds = {k: Bound.from_state_dict(v) for k, v in state_dict['bounds'].items()}
-        zdim = state_dict['zdim']
-        return cls(bounds, zdim)
+        udim = state_dict['udim']
+        return cls(bounds, udim)
