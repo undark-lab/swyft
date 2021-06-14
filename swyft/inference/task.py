4122b366ddba168c9cfd95591dfdfe06978c4cf8
Christoph Weniger <c.weniger@uva.nl>
2021-06-14 21:55:50 +0200

    Bugfix

--- swyft/inference/task.py
+++ swyft/inference/task.py
@@ -41,7 +41,6 @@ class Task:
         self.add = self.posteriors.add
         self.sample = self.posteriors.sample
         self.to = self.posteriors.to
-        self.device = self.posteriors.device
 
     def simulate(self):
         self.dataset.simulate()
