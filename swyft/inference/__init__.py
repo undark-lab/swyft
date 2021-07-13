4761b2a5ed2da56a27bff1ba7dd8402072ac0d11
Christoph Weniger <c.weniger@uva.nl>
2021-07-13 23:12:28 +0200

    Updates init

--- swyft/inference/__init__.py
+++ swyft/inference/__init__.py
@@ -2,4 +2,4 @@ from swyft.inference.posteriors import Posteriors
 from swyft.inference.ratios import IsolatedRatio
 from swyft.inference.train import TrainOptions
 
-__all__ = ["Posteriors", "IsolatedRatio", "TrainOptions"]
+__all__ = ["Posteriors"]
