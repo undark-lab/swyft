45674c1fccf8841b6eb827b6f5ae37e16311adef
Christoph Weniger <c.weniger@uva.nl>
2022-08-24 00:31:35 +0200

    Making plotting work
    - Introducing plot_1d, plot_2d and corner
    - Works now with list of LogRatios objects
    - Unsqueezed output shape of 1dim ratio estimator
    
    	new file:   0C-LogRatioEstimator_Ndim.ipynb
    	modified:   ../swyft/__init__.py
    	modified:   ../swyft/plot/__init__.py
    	new file:   ../swyft/plot/plot2.py

--- swyft/__init__.py
+++ swyft/__init__.py
@@ -4,6 +4,7 @@ from swyft.lightning.estimators import *
 from swyft.lightning.samples import *
 from swyft.lightning.simulator import *
 from swyft.lightning.stores import *
+from swyft.plot import *
 
 try:
     from .__version__ import version as __version__
--- swyft/plot/__init__.py
+++ swyft/plot/__init__.py
@@ -1,14 +1,17 @@
-from swyft.plot.constraint import diagonal_constraint, lower_constraint
-from swyft.plot.histogram import corner, hist1d
-from swyft.plot.mass import empirical_z_score_corner, plot_empirical_z_score
-from swyft.plot.violin import violin
+#from swyft.plot.constraint import diagonal_constraint, lower_constraint
+#from swyft.plot.histogram import corner, hist1d
+#from swyft.plot.mass import empirical_z_score_corner, plot_empirical_z_score
+#from swyft.plot.violin import violin
+from swyft.plot.plot2 import plot_1d, plot_2d, corner
 
 __all__ = [
+    "plot_1d",
+    "plot_2d",
     "corner",
-    "diagonal_constraint",
-    "hist1d",
-    "lower_constraint",
-    "plot_empirical_z_score",
-    "empirical_z_score_corner",
-    "violin",
+#    "diagonal_constraint",
+#    "hist1d",
+#    "lower_constraint",
+#    "plot_empirical_z_score",
+#    "empirical_z_score_corner",
+#    "violin",
 ]
