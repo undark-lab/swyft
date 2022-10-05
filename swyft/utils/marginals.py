477bd11ce4e97ef13c002e5c06045cde8913f7f4
Christoph Weniger <c.weniger@uva.nl>
2022-10-05 09:35:55 +0200

    Fix some dependencies and imports
    	modified:   __init__.py
    	modified:   marginals.py

--- swyft/utils/marginals.py
+++ swyft/utils/marginals.py
@@ -4,15 +4,15 @@
 ##from pandas.core.frame import DataFrame
 #from toolz import keyfilter
 #
-#from swyft.types import (
-#    Array,
-#    MarginalIndex,
-#    MarginalToArray,
-#    #MarginalToDataFrame,
-#    StrictMarginalIndex,
-#)
+from swyft.types import (
+    Array,
+    MarginalIndex,
+    MarginalToArray,
+    #MarginalToDataFrame,
+    StrictMarginalIndex,
+)
 #from swyft.utils.array import tensor_to_array
-#from swyft.utils.misc import depth
+from swyft.utils.misc import depth
 
 
 #def get_d_dim_marginal_indices(n_parameters: int, d: int) -> StrictMarginalIndex:
@@ -35,36 +35,36 @@
 #    return marginal_indices_1d, marginal_indices_2d
 
 
-#def tupleize_marginal_indices(marginal_indices: MarginalIndex) -> StrictMarginalIndex:
-#    """Reformat input marginal_indices into sorted and hashable standard form: tuples of tuples.
-#
-#    a lone input tuple will be respected as coming from the same marginal
-#    lists will assumed to be collections of different marginals
-#    """
-#    if isinstance(marginal_indices, int):
-#        out = [marginal_indices]
-#    elif isinstance(marginal_indices, tuple):
-#        d = depth(marginal_indices)
-#        if d == 0:
-#            raise ValueError("how did this happen?")
-#        elif d == 1:
-#            return (marginal_indices,)
-#        elif d == 2:
-#            return marginal_indices
-#        else:
-#            raise ValueError("marginals can only have two layers of depth, no more.")
-#    else:
-#        out = list(marginal_indices)
-#
-#    for i in range(len(out)):
-#        if isinstance(out[i], int):
-#            out[i] = (out[i],)
-#        else:
-#            out[i] = tuple(sorted(set(out[i])))
-#    out = tuple(sorted(out))
-#    return out
-#
-#
+def tupleize_marginal_indices(marginal_indices: MarginalIndex) -> StrictMarginalIndex:
+    """Reformat input marginal_indices into sorted and hashable standard form: tuples of tuples.
+
+    a lone input tuple will be respected as coming from the same marginal
+    lists will assumed to be collections of different marginals
+    """
+    if isinstance(marginal_indices, int):
+        out = [marginal_indices]
+    elif isinstance(marginal_indices, tuple):
+        d = depth(marginal_indices)
+        if d == 0:
+            raise ValueError("how did this happen?")
+        elif d == 1:
+            return (marginal_indices,)
+        elif d == 2:
+            return marginal_indices
+        else:
+            raise ValueError("marginals can only have two layers of depth, no more.")
+    else:
+        out = list(marginal_indices)
+
+    for i in range(len(out)):
+        if isinstance(out[i], int):
+            out[i] = (out[i],)
+        else:
+            out[i] = tuple(sorted(set(out[i])))
+    out = tuple(sorted(out))
+    return out
+
+
 #def get_marginal_dim_by_key(key: Tuple[int]) -> int:
 #    return len(key)
 #
