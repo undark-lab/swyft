a00108983f8265357312a56eaffc1fab39b20660
Christoph Weniger <c.weniger@uva.nl>
2023-04-17 16:29:58 +0200

    Updates primer main examples

--- notebooks/primer/main_example/simulators.py
+++ notebooks/primer/main_example/simulators.py
@@ -1,6 +1,7 @@
 import numpy as np
 from scipy import stats
 import swyft
+import torch
 
 class SimulatorLinePattern(swyft.Simulator):
     def __init__(self, Npix = 256, bounds = None):
@@ -27,3 +28,38 @@ class SimulatorLinePattern(swyft.Simulator):
     def build(self, graph):
         z = graph.node('z', self.z_sampler)
         mu = graph.node('mu', self.templates, z)
+        
+        
+class SimulatorBlob(swyft.Simulator):
+    def __init__(self, bounds = None, Npix = 64):
+        super().__init__()
+        self.transform_samples = swyft.to_numpy32
+        self.bounds = bounds
+        self.weights, self.dist, self.Cov = self.setup_cov(Npix = Npix)
+        self.Npix = Npix
+
+    def setup_cov(self, Npix = 64):
+        N = Npix**2
+        l = torch.linspace(-1, 1, Npix)
+        L1, L2 = torch.meshgrid(l, l)
+        L = torch.stack([L1, L2], dim = -1)
+        T = L.unsqueeze(0).unsqueeze(0) - L.unsqueeze(2).unsqueeze(2)
+        T = (T**2).sum(-1)**0.5
+        T = T.view(N, N)
+        Cov = torch.exp(-T/.5)*0.5 + torch.exp(-T/0.5)*.25 + torch.exp(-T/2)*.125
+        Cov *= 2
+        dist = torch.distributions.MultivariateNormal(torch.zeros(N), Cov)
+        R = (L1**2+L2**2)**0.5
+        weights = torch.exp(-0.5*R**2/0.3**2)
+        return weights, dist, Cov
+        
+    def sample_GP(self):
+        if self.bounds is None:
+            return self.dist.sample(torch.Size([1]))[0].numpy().reshape(self.Npix, self.Npix)
+        else:
+            i = np.random.randint(len(self.bounds))
+            return self.bounds[i]
+            
+    def build(self, graph):
+        z = graph.node("z", lambda: self.sample_GP())
+        mu = graph.node("mu", lambda z: self.weights*np.exp(z), z)
