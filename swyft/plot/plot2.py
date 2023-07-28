412ff05c3a5703f9668181af8bcf7981b7f9f238
Christoph Weniger <c.weniger@uva.nl>
2023-07-29 00:26:39 +0200

    Remove old functions from plot2

--- swyft/plot/plot2.py
+++ swyft/plot/plot2.py
@@ -19,7 +19,7 @@ from typing import (
 )
 
 
-def grid_interpolate_samples(x, y, bins=1000, return_norm=False):
+def _grid_interpolate_samples(x, y, bins=1000, return_norm=False):
     idx = np.argsort(x)
     x, y = x[idx], y[idx]
     x_grid = np.linspace(x[0], x[-1], bins)
@@ -32,7 +32,7 @@ def grid_interpolate_samples(x, y, bins=1000, return_norm=False):
         return x_grid, y_grid_normed
 
 
-def get_HDI_thresholds(x, cred_level=[0.68268, 0.95450, 0.99730]):
+def _get_HDI_thresholds(x, cred_level=[0.68268, 0.95450, 0.99730]):
     x = x.flatten()
     x = np.sort(x)[::-1]  # Sort backwards
     total_mass = x.sum()
@@ -80,7 +80,7 @@ def plot_2d(
     #    if smooth is not None:
     #        counts = gaussian_filter(counts, smooth)
 
-    levels = sorted(get_HDI_thresholds(counts))
+    levels = sorted(_get_HDI_thresholds(counts))
     ax.contour(
         counts.T,
         extent=[xbins.min(), xbins.max(), ybins.min(), ybins.max()],
@@ -149,9 +149,9 @@ def plot_1d(
     if ax is None:
         ax = plt.gca()
 
-    levels = sorted(get_HDI_thresholds(v))
+    levels = sorted(_get_HDI_thresholds(v))
     if contours:
-        contour1d(zm, v, levels, ax=ax, color=color)
+        _contour1d(zm, v, levels, ax=ax, color=color)
     ax.plot(zm, v, color=color)
     ax.set_xlim([zm.min(), zm.max()])
     ax.set_ylim([-v.max() * 0.05, v.max() * 1.1])
@@ -167,179 +167,6 @@ def plot_1d(
 #    return dict(
 #        mean=mean, mode=mode, HDI1=int2, HDI2=int1, HDI3=int0, entropy=entropy
 #    )
-
-
-def plot_posterior(
-    samples,
-    pois,
-    weights_key=None,
-    ax=None,
-    grid_interpolate=False,
-    bins=100,
-    color="k",
-    contours=True,
-    **kwargs
-):
-    if ax is None:
-        ax = plt.gca()
-
-    if isinstance(pois, int):
-        pois = (pois,)
-
-    w = None
-
-    # FIXME: Clean up ad hoc code
-    if weights_key is None:
-        weights_key = tuple(sorted(pois))
-    try:
-        w = samples["weights"][tuple(weights_key)]
-    except KeyError:
-        if len(weights_key) == 1:
-            for k in samples["weights"].keys():
-                if weights_key[0] in k:
-                    weights_key = k
-                    break
-            w = samples["weights"][tuple(weights_key)]
-        elif len(weights_key) == 2:
-            for k in samples["weights"].keys():
-                if set(weights_key).issubset(k):
-                    weights_key = k
-                    w = samples["weights"][k]
-    if w is None:
-        return
-
-    if len(pois) == 1:
-        x = samples["v"][:, pois[0]]
-
-        if grid_interpolate:
-            # Grid interpolate samples
-            log_prior = samples["log_priors"][pois[0]]
-            w_eff = np.exp(np.log(w) + log_prior)  # p(z|x) = r(x, z) p(z)
-            zm, v = grid_interpolate_samples(x, w_eff)
-        else:
-            v, e = np.histogram(x, weights=w, bins=bins, density=True)
-            zm = (e[1:] + e[:-1]) / 2
-
-        levels = sorted(get_HDI_thresholds(v))
-        if contours:
-            contour1d(zm, v, levels, ax=ax, color=color)
-        ax.plot(zm, v, color=color, **kwargs)
-        ax.set_xlim([x.min(), x.max()])
-        ax.set_ylim([-v.max() * 0.05, v.max() * 1.1])
-
-        # Diagnostics
-        mean = sum(w * x) / sum(w)
-        mode = zm[v == v.max()][0]
-        int2 = zm[v > levels[2]].min(), zm[v > levels[2]].max()
-        int1 = zm[v > levels[1]].min(), zm[v > levels[1]].max()
-        int0 = zm[v > levels[0]].min(), zm[v > levels[0]].max()
-        entropy = -simps(v * np.log(v), zm)
-        return dict(
-            mean=mean, mode=mode, HDI1=int2, HDI2=int1, HDI3=int0, entropy=entropy
-        )
-    elif len(pois) == 2:
-        # FIXME: use interpolation when grid_interpolate == True
-        x = samples["v"][:, pois[0]]
-        y = samples["v"][:, pois[1]]
-        counts, xbins, ybins, _ = ax.hist2d(x, y, weights=w, bins=bins, cmap="gray_r")
-        levels = sorted(get_HDI_thresholds(counts))
-        try:
-            ax.contour(
-                counts.T,
-                extent=[xbins.min(), xbins.max(), ybins.min(), ybins.max()],
-                levels=levels,
-                linestyles=[":", "--", "-"],
-                colors=color,
-            )
-        except ValueError:
-            print("WARNING: 2-dim contours not well-defined.")
-        ax.set_xlim([x.min(), x.max()])
-        ax.set_ylim([y.min(), y.max()])
-
-        xm = (xbins[:-1] + xbins[1:]) / 2
-        ym = (ybins[:-1] + ybins[1:]) / 2
-
-        cx = counts.sum(axis=1)
-        cy = counts.sum(axis=0)
-
-        mean = (sum(xm * cx) / sum(cx), sum(ym * cy) / sum(cy))
-
-        return dict(mean=mean, mode=None, HDI1=None, HDI2=None, HDI3=None, entropy=None)
-
-
-def plot_1d_old(
-    samples,
-    pois,
-    truth=None,
-    bins=100,
-    figsize=(15, 10),
-    color="k",
-    labels=None,
-    label_args={},
-    ncol=None,
-    subplots_kwargs={},
-    fig=None,
-    contours=True,
-) -> None:
-    """Make beautiful 1-dim posteriors.
-
-    Args:
-        samples: Samples from `swyft.Posteriors.sample`
-        pois: List of parameters of interest
-        truth: Ground truth vector
-        bins: Number of bins used for histograms.
-        figsize: Size of figure
-        color: Color
-        labels: Custom labels (default is parameter names)
-        label_args: Custom label arguments
-        ncol: Number of panel columns
-        subplot_kwargs: Subplot kwargs
-    """
-
-    grid_interpolate = False
-    diags = {}
-
-    if ncol is None:
-        ncol = len(pois)
-    K = len(pois)
-    nrow = (K - 1) // ncol + 1
-
-    if fig is None:
-        fig, axes = plt.subplots(nrow, ncol, figsize=figsize, **subplots_kwargs)
-    else:
-        axes = fig.get_axes()
-    lb = 0.125
-    tr = 0.9
-    whspace = 0.15
-    fig.subplots_adjust(
-        left=lb, bottom=lb, right=tr, top=tr, wspace=whspace, hspace=whspace
-    )
-
-    if labels is None:
-        labels = [samples["parameter_names"][pois[i]] for i in range(K)]
-
-    for k in range(K):
-        if nrow == 1 and ncol > 1:
-            ax = axes[k]
-        elif nrow == 1 and ncol == 1:
-            ax = axes
-        else:
-            i, j = k % ncol, k // ncol
-            ax = axes[j, i]
-        ret = plot_posterior(
-            samples,
-            pois[k],
-            ax=ax,
-            grid_interpolate=grid_interpolate,
-            color=color,
-            bins=bins,
-            contours=contours,
-        )
-        ax.set_xlabel(labels[k], **label_args)
-        if truth is not None:
-            ax.axvline(truth[pois[k]], ls=":", color="r")
-        diags[(pois[k],)] = ret
-    return fig, diags
 
 
 def corner(
@@ -456,7 +283,7 @@ def corner(
     return fig
 
 
-def contour1d(z, v, levels, ax=plt, linestyles=None, color=None, **kwargs):
+def _contour1d(z, v, levels, ax=plt, linestyles=None, color=None, **kwargs):
     y0 = -1.0 * v.max()
     y1 = 5.0 * v.max()
     ax.fill_between(z, y0, y1, where=v > levels[0], color=color, alpha=0.1)
@@ -468,10 +295,6 @@ def contour1d(z, v, levels, ax=plt, linestyles=None, color=None, **kwargs):
     #    zero_crossings = np.where(np.diff(np.sign(v-l*1.001)))[0]
     #    for c in z[zero_crossings]:
     #        ax.axvline(c, ls=linestyles[i], color = colors[i], **kwargs)
-
-
-if __name__ == "__main__":
-    pass
 
 
 def plot_zz(
@@ -513,15 +336,5 @@ def plot_pp(
     # swyft.plot.mass.plot_empirical_z_score(ax, cov[:,0], cov[:,1], cov[:,2:])
 
 
-# def plot_scores(mass):
-#    s = mass.get_z_scores()
-#    for j, k in enumerate(s.keys()):
-#        for i in [0, 1, 2]:
-#            y = s[k][i,1]
-#            yerr = np.array([s[k][i,2]-y, y-s[k][i,0]]).reshape(2, 1)
-#            plt.errorbar(j, [y], yerr = yerr, marker='.', color='k')
-#    labels = [list(v) for v in s.keys()]
-#    plt.xticks(range(len(labels)), labels)
-#    plt.axhline(1., color='k', ls=':')
-#    plt.axhline(2., color='k', ls=':')
-#    plt.axhline(3., color='k', ls=':')
+if __name__ == "__main__":
+    pass
