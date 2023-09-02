f44f1c3f029168d8709f0ce58eb76632a9751c25
Christoph Weniger <c.weniger@uva.nl>
2023-09-02 07:58:31 +0200

    Clean up mass.py

--- swyft/plot/__init__.py
+++ swyft/plot/__init__.py
@@ -1,29 +1,9 @@
-# from swyft.plot.constraint import diagonal_constraint, lower_constraint
-# from swyft.plot.histogram import corner, hist1d
-# from swyft.plot.mass import empirical_z_score_corner, plot_empirical_z_score
-# from swyft.plot.violin import violin
 from swyft.plot.plot import (
     plot_posterior,
-    _plot_2d,
     plot_corner,
     plot_zz,
     plot_pp,
-    _plot_1d,
     plot_pair,
 )
 
-__all__ = [
-    "_plot_1d",
-    "_plot_2d",
-    "plot_corner",
-    "plot_zz",
-    "plot_pp",
-    "plot_posterior",
-    "plot_pair"
-    #    "diagonal_constraint",
-    #    "hist1d",
-    #    "lower_constraint",
-    #    "plot_empirical_z_score",
-    #    "empirical_z_score_corner",
-    #    "violin",
-]
+__all__ = ["plot_corner", "plot_zz", "plot_pp", "plot_posterior", "plot_pair"]
