a9f48b471333884a56fc6006841aebc89c3d6861
Christoph Weniger <c.weniger@uva.nl>
2023-09-16 21:50:39 +0200

    Removed UNet submodule
    It turns out that installing submodules as part of pip is not trivial, and
    causese all kinds of problems.  We keep external packages just external for
    now, they can be installed separately if needed.

--- swyft/deps/__init__.py
+++ swyft/deps/__init__.py
@@ -1,11 +1,5 @@
 from swyft.deps.torch_cg.cg_batch import CG
 
-try:
-    from swyft.deps.pytorch_unet.unet import UNet
-except ImportError:
-    UNet = "To access UNet, please install submodules with `git submodule update --init --recursive`."
-
 __all__ = [
-    "UNet",
     "CG",
 ]
